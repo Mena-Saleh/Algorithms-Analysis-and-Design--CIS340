bc3dace0f608c715923d9f8f674fb8ebff93d271
Mena-Ibrahim <Mena.a.saleh.2001@gmail.com>
2023-06-16 17:51:23 +0300

    Added Sorting Algorithms

---
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Insertion_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random list:
+            List<double> list = GenerateRandomList(1000000);
+
+
+            //Try sorting algorithm here:
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            QuickSort(ref list);
+            timer.Stop();
+
+            //Print result:
+            //foreach (double item in list)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine();
+
+            Console.WriteLine("Elapsed time is " + timer.ElapsedMilliseconds + "ms");
+            Console.ReadLine();
+        }
+
+
+
+
+        //Best case -> O[n], the list is sorted already.
+        //Worst case -> O[n^2], the inner loop is always executed for almost n times (list is sorted descendingly).
+        //If the list is almost sorted (I.E: one or few elements are out of order) Insertion sort is a great choice because it is almost O[n]
+        //In place
+        //Fastest sort on small inputs, N < 50
+        public static void InsertionSort(ref List<double> toSort)
+        {
+            double current;
+            int j;
+            for (int i = 1; i < toSort.Count; i++)
+            {
+                current = toSort[i]; //Store current list item to be positioned in a sorted manner.
+                j = i; //Determines the new position of the current list item.
+                while (j - 1 >= 0 && current < toSort[j - 1])
+                {
+                    toSort[j] = toSort[j - 1];
+                    j--;
+                }
+                toSort[j] = current;
+            }
+        }
+
+
+        //Always O(n^2)
+        //In place
+        //Works well for small inputs but otherwise not the best choice
+        //Does around N swaps
+        public static void SelectionSort(ref List<double> toSort) {
+            double min;
+            int minIndex = 0;
+            for (int i = 0; i < toSort.Count; i++)
+            {
+                min = double.MaxValue;
+                for (int j = i; j < toSort.Count; j++) //Get the minimum element
+                {
+                    if (toSort[j] <= min)
+                    {
+                        min = toSort[j];
+                        minIndex = j;
+                    }
+                }
+                Swap(ref toSort, i, minIndex); //Put the minimum element in place.
+            }
+        }
+
+
+        //Always O(N^2) except when sorted it is O(N) (Because of adding the swapped flag to detect swaps and exit early)
+        //Swaps a lot (around N^2 swaps in the worst case)
+
+        public static void BubbleSort(ref List<double> toSort) {
+
+            bool swapped = true;
+            int i = 0;
+            while (swapped)
+            {
+                swapped = false;
+                for (int j = 0; j < toSort.Count - 1 - i; j++) // - i because each iteration one element is bubbled towards the end, -1 so it doesn't go out of bounds.
+                {
+                    if (toSort[j] > toSort[j+1])
+                    {
+                        Swap(ref toSort, j, j + 1);
+                        swapped= true;
+                    }
+                }
+                i++;
+            }
+        }
+
+
+
+        //Divide and Conquer Approaches:
+
+
+        //Easily parallelized
+        //O(Nlog(N)) Time and O(N) Space Complexity (out place).
+        public static void MergeSort(ref List<double> toSort) {
+            MergeSort(ref toSort, 0, toSort.Count - 1);
+        }
+
+        public static void MergeSort(ref List<double> toSort, int start, int end) 
+        {
+            if (start < end)
+            {
+                int middle = (start + end) / 2;
+                MergeSort(ref toSort, start, middle);
+                MergeSort(ref toSort, middle + 1, end);
+                Merge(ref toSort, start, middle, end);
+            }
+   
+        }
+
+        public static void Merge(ref List<double> toSort, int start, int middle, int end)
+        {
+            //Get the left and right (Two lists to merge)
+            List<double> left = toSort.GetRange(start, middle - start + 1);
+            List<double> right = toSort.GetRange(middle + 1, end - middle);
+
+            //Trick to make copying the remaining parts of the two lists intuitive:
+            left.Add(double.MaxValue);
+            right.Add(double.MaxValue);
+
+            int i = 0; int j = 0;
+            for (int k = start; k <= end; k++)
+            {
+
+                if (left[i] <= right[j])
+                {
+                    toSort[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    toSort[k] = right[j];
+                    j++;
+                }
+            }
+
+        }
+      
+
+        //O(NLogN) in most cases
+        //Worst case is a sorted list and it is O(N^2), but it can be remedied by choosing a random pivot
+        //In place, no extra storage needed
+        public static void QuickSort(ref List<double> toSort) {
+            MergeSort(ref toSort, 0, toSort.Count - 1);
+        }
+
+
+        public static void QuickSort(ref List<double> toSort, int start, int end) {
+            if (start < end)
+            {
+                int splitPoint = RandomizedPartition(ref toSort, start, end);
+                QuickSort(ref toSort, start, splitPoint - 1);
+                QuickSort(ref toSort, splitPoint + 1, end);
+            }
+        
+        }
+
+
+        //Randomize to avoid having a worst case split (the pivot is place as first element or last which results in O(N^2) complexity)
+        public static int RandomizedPartition(ref List<double> toSort, int start, int end)
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(start, end + 1);
+
+            Swap(ref toSort, randomIndex, start);
+
+            return Partition(ref toSort, start, end);
+        }
+
+        public static int Partition(ref List<double> toSort, int start, int end)
+        {
+            double pivotValue = toSort[start];
+            int left = start + 1;
+            int right = end;
+            bool isDone = false;
+            while (!isDone) {
+                while (left <= right && toSort[left] <= pivotValue) left++;
+                while (left <= right && toSort[right] <= pivotValue) right++;
+                if (right < left) isDone = true;
+                else Swap(ref toSort, left, right);
+            }
+
+            Swap(ref toSort, start, right);
+            return right;
+        }
+
+        //Helper functions:
+        public static void Swap(ref List<double> list, int i, int j) { 
+        
+            double temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+
+
+
+        //Generates n random numberes each within 1 -> 1000.
+        public static List<double> GenerateRandomList(int n)
+        {
+            Random random = new Random();
+            List<double> randomList = new List<double>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                double randomValue = random.NextDouble() * (1000 - 1) + 1;
+                randomList.Add(Math.Floor(randomValue));
+            }
+
+            return randomList;
+        }
+
+    }
+}
